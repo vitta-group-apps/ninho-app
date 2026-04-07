a555cbfe05c95651a063e4b0527ab448e9eec33c
vitta-group-apps <amomimostore@gmail.com>
2026-04-07 14:30:50 -0300

    test: minimal app to debug white screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
-  createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  createRoot(rootElement).render(<App />);
 }

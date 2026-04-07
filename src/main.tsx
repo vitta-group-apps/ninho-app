2b1c8326758d65f7b49ae83d37dc03b44e922d8d
vitta-group-apps <amomimostore@gmail.com>
2026-04-07 14:29:52 -0300

    fix: cleaner main.tsx entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
